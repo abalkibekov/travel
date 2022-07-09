cddd5c43cc1b63e36a4e75141d1995cc449f9bce
abalkibekov <a.balkibekov@gmail.com>
2022-07-09 21:04:32 +0400

    init: initial commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let message = 'Score 100/100\n' +
+'- the markup is valid\n' +
+'- the markup is semantic\n' +
+'- the makup matches the figma-layout\n' +
+'- styling is performed with css-usage\n' +
+'- interactivity is done with css and works correctly';
+
+console.log(message);
